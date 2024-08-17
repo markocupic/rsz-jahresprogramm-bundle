7fa1d64614b452960c65c374e13014a3b6446ee8
Marko Cupic <m.cupic@gmx.ch>
2024-08-17 14:38:43 +0200

    Fix undfined array key issue

--- contao/dca/tl_rsz_jahresprogramm_participant.php
+++ contao/dca/tl_rsz_jahresprogramm_participant.php
@@ -72,7 +72,7 @@ $GLOBALS['TL_DCA']['tl_rsz_jahresprogramm_participant'] = [
                 'label'      => &$GLOBALS['TL_LANG']['tl_rsz_jahresprogramm_participant']['delete'],
                 'href'       => 'act=delete',
                 'icon'       => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
             ],
             'show'   => [
                 'label' => &$GLOBALS['TL_LANG']['tl_rsz_jahresprogramm_participant']['show'],
